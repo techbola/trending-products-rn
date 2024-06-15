24b714706c8e75c1c9f4a36d8eeeb2182139db83
Adebola <adebola@poptin.com>
2024-06-15 16:36:46 +0100

    home screen and details screen implementation

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,44 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {FlatList, Pressable, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
-const Home = () => {
+// react navigation
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../App';
+
+import ProductItem from '../components/ProductItem';
+import Separator from '../components/Separator';
+
+import {PRODUCTS_LIST} from '../data/contants';
+
+type HomeProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+const Home = ({navigation}: HomeProps) => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={PRODUCTS_LIST}
+        renderItem={({item}) => (
+          <Pressable
+            onPress={() => navigation.navigate('Details', {product: item})}>
+            <ProductItem product={item} />
+          </Pressable>
+        )}
+        ItemSeparatorComponent={Separator}
+        keyExtractor={item => item.id.toString()}
+      />
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+
+    padding: 12,
+    backgroundColor: '#FFFFFF',
+  },
+});
